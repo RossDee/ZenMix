8ed71805d5f1177bea3f32936c047cfba1278ba7
RossDee <diying@sjtu.edu.cn>
2024-02-15 10:03:26 +0800

    create tamagui.config.ts

--- src/components/SoundPlayFaceplate.tsx
+++
@@ -1,8 +0,0 @@
-import React, { useCallback, useMemo, useRef } from 'react'
-import { View, Text, StyleSheet, Button } from 'react-native'
-import {
-  BottomSheetModal,
-  BottomSheetModalProvider,
-} from '@gorhom/bottom-sheet'
-
-const SoundPlayFacePlate = () => {}
